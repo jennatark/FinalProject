d79faae733d4a67cfde2b34e08c3bd79cf05e5cc
jennatark <jennartark@gmail.com>
2020-10-26 17:44:01 -0700

    update final.js

--- final.js
+++ final.js
@@ -1,6 +1,17 @@
 //Things we need to do:
 //javascript code to randomize workouts
 //index page that shows title, instructions and list of all
+var randRep = 0;
+var randInt = 0;
+var result = "";
 fucntion randExer() {
-    
+  var listExcercise = ["Push-Ups" , "Sit-Ups". "Jumping Jacks", "Plank", "Side Plank", "V-Ups", "Burpees", "Lunges",
+                      "Side Lunges", "High Knees", "Donkey Kicks", "Inchworm", "Tuck Jumps","Mountain Climber", "Wall Sit",
+                      "Calf Raise", "Superman", "Arm Circles", "Russian Twist", "Bicycles", "Crunches", "Downward Dog",
+                      "Single Leg Dead Lifts", "Sprints", "Pump Squats"];
+    randInt = Math.floor(Math.random() * 25);
+    randRep = Math.floor(Math.random() + 1 * 60);
+    result = "Do " + listExcercise[randInt] + " for " + randRep + " seconds";
+    document.getElementById("result").innerHTML = result:
+
 }
