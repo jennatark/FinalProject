0fbd0c06c9e82949ee03c215f804ed793641d6ea
jennatark <jennartark@gmail.com>
2020-10-26 18:09:52 -0700

    still testing radnExer

--- final.js
+++ final.js
@@ -5,13 +5,13 @@ var randRep = 0;
 var randInt = 0;
 
 function randExer() {
-  var listExcercise = ["Push-Ups" , "Sit-Ups". "Jumping Jacks", "Plank", "Side Plank", "V-Ups", "Burpees", "Lunges",
+  var listExercise = ["Push-Ups" , "Sit-Ups". "Jumping Jacks", "Plank", "Side Plank", "V-Ups", "Burpees", "Lunges",
                       "Side Lunges", "High Knees", "Donkey Kicks", "Inchworm", "Tuck Jumps","Mountain Climber", "Wall Sit",
                       "Calf Raise", "Superman", "Arm Circles", "Russian Twist", "Bicycles", "Crunches", "Downward Dog",
                       "Single Leg Dead Lifts", "Sprints", "Pump Squats"];
     randInt = Math.floor(Math.random() * 25);
     randRep = Math.floor(Math.random() + 1 * 60);
-    result = "Do " + listExcercise[randInt] + " for " + randRep + " seconds";
+    result = "Do " + listExercise[randInt] + " for " + randRep + " seconds";
     document.getElementById("result").innerHTML = result;
 
 }
