475ac4a7b235e52dde34ed60729677cf447262c4
jennatark <jennartark@gmail.com>
2020-10-26 18:32:56 -0700

    added repNums array

--- final.js
+++ final.js
@@ -9,9 +9,9 @@ function randExer() {
                       "Side Lunges", "High Knees", "Donkey Kicks", "Inchworm", "Tuck Jumps","Mountain Climber", "Wall Sit",
                       "Calf Raise", "Superman", "Arm Circles", "Russian Twist", "Bicycles", "Crunches", "Downward Dog",
                       "Single Leg Dead Lifts", "Sprints", "Pump Squats"];
+    var repsNums = ["10","15","20","25","30","35","40","45","50","55","60"];
     randInt = Math.floor(Math.random() * 25);
     randRep = Math.floor(Math.random() + 1 * 60);
-    result = "Do " + listExercise[randInt] + " for " + randRep + " seconds";
+    result = "Do " + listExercise[randInt] + " for " + repsNums[randRep] + " seconds";
     document.getElementById("result").innerHTML = result;
-
 }
