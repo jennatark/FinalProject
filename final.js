5da93831ae2271be89dffcbbc61277943c2ff052
jennatark <jennartark@gmail.com>
2020-10-26 19:12:43 -0700

    added plurals

--- final.js
+++ final.js
@@ -2,9 +2,9 @@
 //javascript code to randomize workouts
 //index page that shows title, instructions and list of all
 function randExer() {
-    var listExercise = ["Push-Ups" , "Sit-Ups", "Jumping Jacks", "Plank", "Side Plank", "V-Ups", "Burpees", "Lunges",
-                      "Side Lunges", "High Knees", "Donkey Kicks", "Inchworm", "Tuck Jumps","Mountain Climber", "Wall Sit",
-                      "Calf Raise", "Superman", "Arm Circles", "Russian Twist", "Bicycles", "Crunches", "Downward Dog",
+    var listExercise = ["Push-Ups" , "Sit-Ups", "Jumping Jacks", " A Plank", " A Side Plank", "V-Ups", "Burpees", "Lunges",
+                      "Side Lunges", "High Knees", "Donkey Kicks", "Inchworms", "Tuck Jumps","Mountain Climbers", "A Wall Sit",
+                      "Calf Raise", "Supermans", "Arm Circles", "Russian Twist", "Bicycles", "Crunches", "Downward Dog",
                       "Single Leg Dead Lifts", "Sprints", "Pump Squats"];
     var repsNums = ["10","15","20","25","30","35","40","45","50","55","60","65","70","75","80","85","90"];
     var randInt = Math.floor(Math.random() * 25);
