f0328ec7d34dead593131d7666e0e36afb439130
jennatark <70237531+jennatark@users.noreply.github.com>
2020-10-28 18:30:19 -0700

    Update final.js

--- final.js
+++ final.js
@@ -1,6 +1,4 @@
-//Things we need to do:
-//javascript code to randomize workouts
-//index page that shows title, instructions and list of all
+var randRep;
 function randExer() {
     var listExercise = ["Push-Ups" , "Sit-Ups", "Jumping Jacks", " A Plank", " A Side Plank", "V-Ups", "Burpees", "Lunges",
                       "Side Lunges", "High Knees", "Donkey Kicks", "Inchworms", "Tuck Jumps","Mountain Climbers", "A Wall Sit",
@@ -8,9 +6,10 @@ function randExer() {
                       "Single Leg Dead Lifts", "Sprints", "Pump Squats"];
     var repsNums = ["10","15","20","25","30","35","40","45","50","55","60","65","70","75","80","85","90"];
     var randInt = Math.floor(Math.random() * 25);
-    var randRep = Math.floor(Math.random() * 17);
+    randRep = Math.floor(Math.random() * 17);
     var result = "Do " + listExercise[randInt] + " for " + repsNums[randRep] + " seconds";
     document.getElementById("result").innerHTML = result;
+    document.getElementById("countdown").innerHTML = repsNums[randRep];
 }
 
 function randMotiv() {
@@ -32,4 +31,3 @@ function randMotiv() {
   document.getElementById("motivational").innerHTML = quote;
 
 }
-
